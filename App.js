d3b05430cc71588f8d4da8b047e65cbed591befa
aswitha123 <94103731+aswitha123@users.noreply.github.com>
2023-11-09 10:16:41 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!!!!!</h1>
+      <h2>welcome to react programming</h2>
+    </div>
+  );
+}
